362a68e669e5f665b1c9ec45c68fd48c4733d50d
Scott Hoisington <hoyzington@gmail.com>
2020-10-21 23:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ BoardMember.prototype.approve = function() {
 };
 
 BoardMember.prototype.doCharity = function() {
-  return '';
+  return 'I like to help people';
 };
 
 BoardMember.prototype.releasePressStatement = function() {
